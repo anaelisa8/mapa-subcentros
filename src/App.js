1694353223609565f58685e93b10d2c9396b351e
Alex Vargas <alexjesusvargas@gmail.com>
2021-10-04 11:07:29 -0500

    added legend to map

--- src/App.js
+++ src/App.js
@@ -10,6 +10,49 @@ function App() {
       </header>
       <div className="body">
         <Deck />
+        <div id="foo">
+          
+          <table class="topazCells">
+            <tbody>
+              <tr>
+                <td>#</td>
+                <td>Tópico</td>
+              </tr>
+              <tr>
+                <td>Tópico 0:</td>
+                <td>Centro comercial e industrial</td>
+              </tr>
+              <tr>
+                <td>Tópico 2:</td>
+                <td>Centro financiero y comercial</td>
+              </tr>
+              <tr>
+                <td>Tópico 4:</td>
+                <td>Centro industrial y comercial</td>
+              </tr>
+              <tr>
+                <td>Tópico 5:</td>
+                <td>Centro de comercio al por menor</td>
+              </tr>
+              <tr>
+                <td>Tópico 7:</td>
+                <td>Centro de refracciones y gubernamental</td>
+              </tr>
+              <tr>
+                <td>Tópico 9:</td>
+                <td>Centro industrial y de refracciones</td>
+              </tr>
+              <tr>
+                <td>Tópico 11:</td>
+                <td>Centro médico, educativo y financiero</td>
+              </tr>
+              <tr>
+                <td>Tópico 12:</td>
+                <td>Centro comercial y educativo</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
