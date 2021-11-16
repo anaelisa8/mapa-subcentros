411db787f470b0b1e72d6d1efa736dafaf5ed55c
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-16 17:42:48 -0600

    changed table style

--- src/App.js
+++ src/App.js
@@ -12,34 +12,44 @@ function App() {
           <table class="topazCells">
             <tbody>
               <tr>
+              <td id="tdTop">Tipo</td>
                 <td id="tdTop">Tipología de Subcentros</td>
               </tr>
               <tr>
-                <td id="td0">Tipo 1: Enfoque adicional en mantenimiento, servicios educativos y ferreterías</td>
+                <td id="td0">1</td>
+                <td id="td0">Enfoque adicional en mantenimiento, servicios educativos y ferreterías</td>
               </tr>
               <tr>
-                <td id="td1">Tipo 2: Enfoque adicional en servicios profesionales y esparcimiento</td>
+              <td id="td1">2</td>
+                <td id="td1">Enfoque adicional en servicios profesionales y esparcimiento</td>
               </tr>
               <tr>
-                <td id="td2">Tipo 3: Actividades financieras</td>
+              <td id="t2">3</td>
+                <td id="td2">Actividades financieras</td>
               </tr>
               <tr>
-                <td id="td4">Tipo 4: Reparación y mantenimiento de equipo y maquinaría</td>
+              <td id="td4">4</td>
+                <td id="td4">Reparación y mantenimiento de equipo y maquinaría</td>
               </tr>
               <tr>
-                <td id="td5">Tipo 5: Enfoque adicional en tiendas de ropa, calzado, esparcimiento</td>
+              <td id="td5">5</td>
+                <td id="td5">Enfoque adicional en tiendas de ropa, calzado, esparcimiento</td>
               </tr>
               <tr>
-                <td id="td7">Tipo 6: Taller y refacciones para el sector automotriz</td>
+              <td id="td7">6</td>
+                <td id="td7">Taller y refacciones para el sector automotriz</td>
               </tr>
               <tr>
-                <td id="td9">Tipo 7: Reparación y mantenimiento. Actividades áreas agropecuarias y forestales</td>
+              <td id="td9">7</td>
+                <td id="td9">Reparación y mantenimiento. Actividades áreas agropecuarias y forestales</td>
               </tr>
               <tr>
-                <td id="td11">Tipo 8: Servicios médicos y actividad científica</td>
+              <td id="td11">8</td>
+                <td id="td11">Servicios médicos y actividad científica</td>
               </tr>
               <tr>
-                <td id="td12">Tipo 9: Enfoque en ropa y calzado, esparcimeinto y servicios profesionales</td>
+              <td id="td12">9</td>
+                <td id="td12">Enfoque en ropa y calzado, esparcimeinto y servicios profesionales</td>
               </tr>
             </tbody>
           </table>
